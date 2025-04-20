0319562693dda7d032f63f5adad71a045095421d
LeviF <electrosquib@gmail.com>
2025-04-20 07:09:37 -1000

    Cors stuff

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import subscribe_email, run_migrations, run_collectstatic, create_superuser_view
+from main.views import subscribe_email
 
 
 urlpatterns = [
